ed8d14575e2b7ec74a52b8f9bcc656250f11ae2e
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-02-17 15:58:53 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
 namespace game {
-    export enum kind {
-        //% block
+    export enum Kind {
+        //% block="player"
+        Player,
+        //% block="enemy"
+        Enemy,
+        //% block="food"
+        Food,
+        //% block="colletible"
+        Colletible
+    }
+    let newSprite
+    //% block="create sprite %sprite of kind %kind"
+    export function setSprite(sprite: string, kind: Kind){
+        newSprite = kind
+        game.createSprite(2, 2)
     }
 }
