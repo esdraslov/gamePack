5bb5be2650049a355b0d64e0cbae3be6cf98a255
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-02-17 16:46:54 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -17,12 +17,12 @@ namespace game {
     //% block="show life %lifes"
     //% lifes.min=1 lifes.max=7
     export function showLife(lifes: number) {
-        for(let i = 0; i < lifes; i++){
+        for(let i = 1; i < lifes; i++){
             led.plot(i, 0)
         }
     }
     //% block="set life %value"
-    //% value.min=1 value.max=7
+    //% value.min=0 value.max=7
     function setLife(value: number) {
         game.setLife(value)
     }
