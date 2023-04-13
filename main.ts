e342a78e568266ed8970b69c833171f290d108cf
esdraslov <103470150+esdraslov@users.noreply.github.com>
2023-04-13 18:14:56 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -23,7 +23,7 @@ namespace game {
     }
 }
 namespace serial {
-    //% block="serial view player score"
+    //% block="serial read player score"
     export function serialGamePoints () {
         serial.writeValue("score", game.score())
     }
